7797643dfc681125242f8f018482576f45003823
Sumit <dev.sumitthakur@gmail.com>
2019-01-03 01:12:33 -0800

    changes for layout adjustment

--- app/src/main/java/st/prestoq/api/ApiService.kt
+++ app/src/main/java/st/prestoq/api/ApiService.kt
@@ -1,11 +1,13 @@
 package st.prestoq.api
 
+import android.util.Log
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.observers.DisposableObserver
 import io.reactivex.schedulers.Schedulers
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import st.prestoq.viewmodel.model.ApiResponse
 
@@ -16,7 +18,8 @@ class ApiService {
     }
 
     companion object {
-        private val baseUrl = "https://prestoq.com/android-coding-challenge"
+        private val baseUrl = "https://prestoq.com/"
+        private val fullUrl = "https://prestoq.com/android-coding-challenge"
 
         lateinit var dataService: ApiServiceInterface
         private var instance: ApiService? = null
@@ -33,6 +36,7 @@ class ApiService {
                 .baseUrl(baseUrl)
                 .client(clientBuilder.build())
                 .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build()
 
         dataService = retrofit.create<ApiServiceInterface>(ApiServiceInterface::class.java)
@@ -42,19 +46,23 @@ class ApiService {
         var retVal: Observable<ApiResponse>? = null
 
         try {
-            dataService.getManagerSpecials()
+            dataService.getManagerSpecials(fullUrl)
                     .subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
                     .subscribe(object : DisposableObserver<ApiResponse>() {
-                        override fun onComplete() {}
+                        override fun onComplete() {
+                            Log.d(ApiService::class.java.name, "success response complete!")
+                        }
 
                         override fun onNext(value: ApiResponse) {
+                            Log.d(ApiService::class.java.name, "success response: " + value)
                             if (responseListener != null) {
                                 responseListener.onSuccess(value)
                             }
                         }
 
                         override fun onError(e: Throwable) {
+                            Log.e(ApiService::class.java.name, "error response: " + e)
                             if (responseListener != null) {
                                 responseListener.onError("error: ${e.message}")
                             }
